78654075def494252cf3f666544a79cf8d395058
GevorHovh <123312885+GevorHovh@users.noreply.github.com>
2023-02-01 13:25:57 +0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,23 @@
+/*
+function calc(a) {
+    let b = 10;
+    console.log(a + b )
+}
+
+calc(5);
+*/
+
+/*
+let array = [5,10,5,8,7,9,6];
+function calc(sum){
+for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < 1; j++) {
+        sum += array[i];
+        console.log(sum);
+    }
+    
+}
+};
+calc(0)
+*/
+
